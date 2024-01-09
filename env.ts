abeec685bc9fec99e87907649fec98d3ffa58aea
Kenneth Pang <pangkaho8@gmail.com>
2024-01-09 13:26:54 +0800

    updated .env config

--- env.ts
+++ env.ts
@@ -4,10 +4,9 @@ import populateEnv from "populate-env";
 config();
 
 export let env = {
-  DB_NAME: '',
-  DB_USER: '',
-  DB_PASSWORD: '',
-  SESSION_SECRET: ''
-}
+  POSTGRES_DB: "",
+  POSTGRES_USER: "",
+  POSTGRES_PASSWORD: "",
+};
 
 populateEnv(env, { mode: "halt" });
