32f9940cd23a2b31600cb36f2a2d0ed0a5e1b4c4
Jacky Lai <jacky96923@gmail.com>
2024-01-12 15:52:19 +0800

    j same commit

--- migrations/20240111100121_create_styleImages_table.ts
+++ migrations/20240111100121_create_styleImages_table.ts
@@ -2,13 +2,8 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("styleImages", function (table) {
-<<<<<<< HEAD
     table.increments("id").primary();
     table.string("image", 255).unsigned();
-=======
-    table.increments().primary();
-    table.integer("image").unsigned();
->>>>>>> refs/remotes/origin/main
     table
       .integer("styleList_id")
       .unsigned()
