2c23864b6ef3673edc77dbc2d62037154563500d
Brian Chan <brianchan1997.cc@gmail.com>
2024-01-16 11:00:09 +0800

    modified booking timeslot v1

--- userService.ts
+++ userService.ts
@@ -29,11 +29,7 @@ export class UserService {
   //Promise<{ man_count: number; c_count: number; } | undefined>
   async booking_timeslot(category: string, dateTime: string) {
     try {
-      if (category === "Haircut Wash Style"){
-        return await this.knex.select("man_count", "c_count").from("roster").where("datetime", dateTime)
-      } else if (category === "Style Perming"){
         return await this.knex.select("man_count", "c_count", "p_count").from("roster").where("datetime", dateTime)
-      }
     } catch (error) {
       
     }
