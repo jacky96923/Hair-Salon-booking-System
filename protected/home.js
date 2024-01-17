97ecdff9741c8fa9f174e7032efa69ad75788112
Jacky Lai <jacky96923@gmail.com>
2024-01-17 17:58:54 +0800

    jacky fuck

--- protected/home.js
+++ protected/home.js
@@ -64,7 +64,7 @@ function renderData(photos) {
   let resultImg = document.getElementById("resultImg");
   for (let photo of photos) {
     let img = document.createElement("img");
-    img.src = photo.filename;
+    img.src = "./" + photo.filename;
     let buttonBooking = document.createElement("button");
     let buttonRemove = document.createElement("button");
     buttonBooking.classList.add("myStyleBooking");
